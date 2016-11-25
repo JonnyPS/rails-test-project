388f850b31c5b6538b60efbbb3dbe0d46aa624ea
Jonny Spry <aml-jonny@AML-MAC013.local>
2016-11-25 11:28:30 +0000

    add a for loop in for a test amend and commit

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,8 +1,17 @@
 class HomepageController < ApplicationController
 
 	def home 
+
 		@greeting  = "rails says: hello world"	
 		# puts "Hello World"
+		$i =0
+		$total = 5
+
+		while $i <= $total do 
+			@iValue = "inside the loop i = #$i"
+			# puts("inside the loop i = #$1" )
+			$i +=1
+		end
 	end
 
 end
